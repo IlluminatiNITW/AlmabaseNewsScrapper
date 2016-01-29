4d2edaf04a8b7ed818600e1a00fbbaf2ddca1476
makalaaneesh <makalaaneesh@yahoo.com>
2016-01-30 01:14:38 +0530

    simple scraper that scrapes article links that have the word -hyderabad- in it

--- tutorial/tutorial/spiders/tutorial_spider.py
+++ tutorial/tutorial/spiders/tutorial_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.spiders import SitemapSpider
+from tutorial.items import HyderabadItem
 
 class TutorialSpider(scrapy.Spider):
     name = "tutorial"
@@ -14,3 +16,20 @@ class TutorialSpider(scrapy.Spider):
             link = sel.xpath('a/@href').extract()
             desc = sel.xpath('text()').extract()
             print title, link, desc
+
+
+
+class YourStorySpider(SitemapSpider):
+    name = 'yourstory'
+    count = 0
+    sitemap_urls = ['http://yourstory.com/sitemap_index.xml']
+
+    def parse(self, response):
+        # print "HIT: ", response.url
+        # print "---------------------------", response.body
+        self.count = self.count + 1
+        if "hyderabad" in response.body.lower():
+            print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            item = HyderabadItem()
+            item['link'] = response.url
+            yield item
