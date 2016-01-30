3e906be0061168f3cec4e7586db9b96c50c69645
karajrish <rishabh.karajgi@gmail.com>
2016-01-31 00:37:43 +0530

    generic rss

--- rss/get_rss_links.py
+++ rss/get_rss_links.py
@@ -2,10 +2,12 @@ import requests
 import feedparser
 from bs4 import BeautifulSoup  
 from time import sleep
+from config import configs
 
 website_list =["http://rss.cnn.com/rss/edition.rss",
 "http://yourstory.com/feed/",
-"http://economictimes.indiatimes.com/rssfeedsdefault.cms"]
+"http://economictimes.indiatimes.com/rssfeedsdefault.cms",
+"https://news.ycombinator.com/rss"]
 
 fo = open("foo.txt", "wb")
 #website_url = "http://yourstory.com/feed/"
@@ -14,13 +16,16 @@ for website_url in website_list:
     d = feedparser.parse(website_url)
 
     for post in d.entries:
+        fo.write(website_url+',')
         print "Time Stamp : "
         print post.published
+        fo.write(post.published+',')
         print "News Website url : "
         print post.summary_detail.base
         print "Article URL : "
-        print post.id
-        fo.write(post.id)
+        link = post[configs[website_url]["link"]]
+        print link
+        fo.write(link)
         fo.write("\n")
         print "Article Title : "
         print post.title
