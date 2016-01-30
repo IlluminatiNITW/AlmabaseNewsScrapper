ef4328e9ecb8279e8125491a07107568a4815df4
acemaster <vivekhtc25@gmail.com>
2016-01-30 23:58:40 +0530

    Completed nitw classification

--- Almabase/reciever.py
+++ Almabase/reciever.py
@@ -1,8 +1,46 @@
+from parser import *
+import requests
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Almabase.settings')
+import django
+
+django.setup()
+from Classifer.models import *
+
 from multiprocessing.connection import Client
 from array import array
 from Queue import Queue
 import threading
 import time
+
+def article_keywords(article):
+    keys=Keywords.objects.get(article=article)
+    # print keys
+    l=[k.keyword for k in keys.keywords.all()]
+    # print " ".join(l)
+    keyset={'keyword':" ".join(l)}
+    return keyset
+
+
+def build_classifier():
+    print "Starting testing of Bayes Classifer"
+    labeled_articles = [(a, a.relevant) for a in Article.objects.all()]
+    print "labeled articles ", labeled_articles
+    featuresets=[]
+    for (article, relevant) in labeled_articles:
+        r=article_keywords(article)
+        featuresets.append((r,relevant))
+
+    # print featuresets
+    train_set = featuresets
+    # print train_set
+    newsTrainer = Trainer(tokenizer)
+    for f in train_set:
+        newsTrainer.train(f[0]['keyword'],f[1])
+    newsClassifier = Classifier(newsTrainer.data, tokenizer)
+    return newsClassifier
+
 
 workQueue = Queue()
 address = ('localhost', 7001)
@@ -18,7 +56,7 @@ class myThread(threading.Thread):
 		message=""
 		while True:
 			message=conn.recv() 
-			print "received", message 
+			print "received", message[0],message[1]
 			q.put(message)
 
     def run(self):
@@ -33,12 +71,19 @@ class myThread2(threading.Thread):
         self.name = name
         self.q = q
     def process_data(self,threadName, q):
-		while True:
-		    if not q.empty():
-		        data = q.get()
-		        q.task_done()
-		        print "%s processing %s" % (threadName, data)
-		    time.sleep(1)
+        while True:
+            if not q.empty():
+                message = q.get()
+                classifier = build_classifier()
+                print "BUilt classifier"
+                url = message[0]
+                r = requests.get(url)
+                print r.status_code
+                sample_thread = Parser_Classifier(url,message[1], classifier)
+                sample_thread.start()
+                q.task_done()
+                print "%s processing %s" % (threadName, message)
+            time.sleep(1)
 
     def run(self):
         print "Starting " + self.name
--- Almabase/server.py
+++ Almabase/server.py
@@ -7,12 +7,13 @@ listener = Listener(address, authkey='secret password')
 
 conn = listener.accept()
 print 'connection accepted from', listener.last_accepted
-
+line=[]
 while True:
 	message=raw_input("Enter url: ")
+	r=requests.get(message)
 	if message == 'quit':
 		break
-	conn.send(message)
+	conn.send([message,r.content])
 
 conn.close()
 listener.close()
