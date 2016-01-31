9b89d9ae073e15dd1980c60c7f0e05b9b36c9bcd
Sreerag Sreenath <sreeragsreenath@gmail.com>
2016-01-31 08:43:03 +0530

    added rishabh files

--- final/final/settings.py
+++ final/final/settings.py
@@ -19,18 +19,18 @@ NEWSPIDER_MODULE = 'final.spiders'
 #USER_AGENT = 'final (+http://www.yourdomain.com)'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS=32
+CONCURRENT_REQUESTS=32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED=False
+COOKIES_ENABLED=False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED=False
--- final/final/spiders/final.py
+++ final/final/spiders/final.py
@@ -1,24 +1,26 @@
 import scrapy
 from scrapy.http import Request
 from scrapy.spider import BaseSpider
+import newspaper
 
-r = open('test.txt', 'r')
-text=r.read()
-lines = text.split("\n")
-print lines
+with open('foo.txt') as r:
+    content = r.readlines()
+
 class DmozSpider(scrapy.Spider):
     name = "htmlget"
-    allowed_domains=[]
     start_urls=[]
     def __init__(self):
-        for currentline in lines:
-            currentline=currentline.split(',')
-            self.allowed_domains.append(currentline[0])
-            self.start_urls.append(currentline[1])
+        for currentline in content:
+            currentline=currentline.split('#')
+            # self.allowed_domains.append(newspaper.urls.get_domain(currentline[2]))
+            self.start_urls.append(currentline[2])
+            # print self.allowed_domains
+            print self.start_urls
     def parse(self, response):
-        print self.allowed_domains
+        # print self.allowed_domains
         print self.start_urls
         filename = response.url.split("/")[-2] + '.html'
+        #filename = response.url + '.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
-        currentline=r.readline().split(",")
+            f.close()
